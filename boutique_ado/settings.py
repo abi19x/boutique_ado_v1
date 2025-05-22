05c11e81cae7e946a6a49c052f4676c91ec0b049
Abel YOHANES <k9ja1xwk@students.codeinstitute.net>
2025-05-22 20:19:58 +0000

    Added sourcecode for product sorting

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a)rumc*m1la%lmbioa*h2@=isp4@m2v7x_m3a5=81dkwv+a@z!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-      '8000-abi19x-boutiqueadov1-9t825k0vcjj.ws-eu118.gitpod.io' 
+      '8000-abi19x-boutiqueadov1-vdlpyu9pccp.ws-eu118.gitpod.io' 
 ]
 
 
