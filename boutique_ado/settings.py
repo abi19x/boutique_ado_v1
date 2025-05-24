e6c284b47311c00b2eb404a64a82da2ae133ee28
Abel YOHANES <k9ja1xwk@students.codeinstitute.net>
2025-05-24 10:40:33 +0000

    Added quantity boxes to shopping bag page

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a)rumc*m1la%lmbioa*h2@=isp4@m2v7x_m3a5=81dkwv+a@z!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-      '8000-abi19x-boutiqueadov1-ohm2xvsbvt5.ws-eu119.gitpod.io' 
+      '8000-abi19x-boutiqueadov1-ilb63guljba.ws-eu119.gitpod.io' 
 ]
 
 
