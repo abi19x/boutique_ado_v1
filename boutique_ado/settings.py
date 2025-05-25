1e036f0ab5a700673255136db44fa56b196b6a00
Abel YOHANES <k9ja1xwk@students.codeinstitute.net>
2025-05-25 23:23:25 +0000

    Added forms, admin and signals

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a)rumc*m1la%lmbioa*h2@=isp4@m2v7x_m3a5=81dkwv+a@z!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-       '8000-abi19x-boutiqueadov1-xy9fvbwgsb9.ws-eu119.gitpod.io' 
+       '8000-abi19x-boutiqueadov1-fl5808c212t.ws-eu119.gitpod.io' 
 ]
 
 
