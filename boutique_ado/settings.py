7d0e0535e8bdea9923b36cb1fb21625c6c6ccc72
abi19x <k9ja1xwk@students.codeinstitute.net>
2025-06-09 11:35:16 +0100

    Move BASE_DIR definition above usage

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -17,13 +17,12 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
